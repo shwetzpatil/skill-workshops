e1d4e3b15c2135f2f78cc59f7e1854555144ac94
shweta patil <shwetapatil@shwetas-mbp.home>
2018-11-22 20:19:09 +0000

    Debugging workshop

--- week-2/mocking_2/spec/exercise1/docking_station_spec.rb
+++ week-2/mocking_2/spec/exercise1/docking_station_spec.rb
@@ -1,23 +1,36 @@
 require 'docking_station'
 
-class MockWorkingBike
-  def working?
-    true
-  end
-end
-
-class MockBrokenBike
-  def working?
-    false
-  end
-end
+# class MockWorkingBike
+#   def working?
+#     true
+#   end
+# end
+#
+# class MockBrokenBike
+#   def working?
+#     false
+#   end
+# end
 
 describe DockingStation do
   describe '#working_bike_count' do
+
+    let (:working_bike) {double ('mock_working_bike')}
+    let (:broken_bike) {double ('mock_broken_bike')}
+    before(:each) {
+      allow(working_bike).to receive(:working?).and_return(true)
+      allow(broken_bike).to receive(:working?).and_return(false)
+    }
+
+
     context '1 working bike' do
+
       it "returns 1" do
+
+
+
         bikes = []
-        bikes << MockWorkingBike.new
+        bikes << working_bike
         docking_station = DockingStation.new(bikes)
 
         expect(docking_station.working_bike_count).to eq(1)
@@ -26,9 +39,10 @@ describe DockingStation do
 
     context '1 working bike, 1 broken bike' do
       it "returns 1" do
+
         bikes = []
-        bikes << MockWorkingBike.new
-        bikes << MockBrokenBike.new
+        bikes << working_bike
+        bikes << broken_bike
         docking_station = DockingStation.new(bikes)
 
         expect(docking_station.working_bike_count).to eq(1)
@@ -37,24 +51,28 @@ describe DockingStation do
   end
 
   describe '#random_bike_working?' do
+
+    let (:working_bike) {double ('mock_working_bike')}
+    let (:broken_bike) {double ('mock_broken_bike')}
+     before(:each) {
+      allow(working_bike).to receive(:working?).and_return(true)
+      allow(broken_bike).to receive(:working?).and_return(false)
+    }
+
     context 'random bike is working' do
-      xit 'returns true' do
-        working_bike = MockWorkingBike.new
-        broken_bike = MockBrokenBike.new
-
-        docking_station = DockingStation.new([working_bike, broken_bike])
-
+      it 'returns true' do
+        bikes = [working_bike, broken_bike]
+        docking_station = DockingStation.new(bikes)
+        allow(bikes).to receive(:sample).and_return(bikes[0])
         expect(docking_station.random_bike_working?).to eq(true)
       end
     end
 
     context 'random bike is broken' do
-      xit 'returns false' do
-        working_bike = MockWorkingBike.new
-        broken_bike = MockBrokenBike.new
-
-        docking_station = DockingStation.new([working_bike, broken_bike])
-
+      it 'returns false' do
+        bikes = [working_bike, broken_bike]
+        docking_station = DockingStation.new(bikes)
+        allow(bikes).to receive(:sample).and_return(bikes[1])
         expect(docking_station.random_bike_working?).to eq(false)
       end
     end
--- week-5/encapsulation_with_constructor_and_prototype_pattern/index.js
+++ week-5/encapsulation_with_constructor_and_prototype_pattern/index.js
@@ -1,1 +1,165 @@
-console.log("hello!");
+//copy to browser and see web dev   console file:///Users/shwetapatil/Desktop/skills-workshops/week-5/encapsulation_with_constructor_and_prototype_pattern/index.html
+
+// console.log("hello!");
+
+// Question 1
+
+// // How does the printed output of the program change if you rename
+// // CountModel to countmodel (everywhere it appears)?
+
+// function countmodel() {
+//   this._count = 0;
+// };
+
+// countmodel.prototype.count = function() {
+//   return this._count;
+// };
+
+// countmodel.prototype.set = function(count) {
+//   this._count = count;
+// };
+
+// var countmodel = new countmodel();
+// countmodel.set(9);
+// console.log("count is", countmodel.count());
+
+
+// question 2
+
+// function CountModel() {
+//   this._count = 0;
+// };
+
+// CountModel.prototype.count = function() {
+//   return this._count;
+// };
+
+// // How does the printed output of the program change if you rename
+// // `set` to `_set` (and change `countModel.set(5)` below to
+// // `countModel._set(5)`)?
+// CountModel.prototype._set = function(count) {
+//   this._count = count;
+// };
+
+// var countModel = new CountModel();
+// // countModel.set(5);
+// countModel._set(5);
+// console.log("count is", countModel.count());
+
+
+//question 3
+
+// function CountModel() {
+//   this._count = 0;
+
+//   // How does the printed output of the program change if you
+//   // uncomment the line below? Why does this happen?
+
+//   // return {};
+// };
+
+// CountModel.prototype.count = function() {
+//   return this._count;
+// };
+
+// CountModel.prototype.set = function(count) {
+//   this._count = count;
+// }
+
+// var countModel = new CountModel();
+// countModel.set(5);
+// console.log("count is", countModel.count());
+
+
+// Question 4
+
+// function CountModel() {
+//   this._count = 0;
+// };
+
+// CountModel.prototype.count = function() {
+//   return this._count;
+// };
+
+// CountModel.prototype.set = function(count) {
+//   this._count = count;
+// };
+
+// var countModel = new CountModel();
+
+// // How does the printed output of the program change if you add this
+// // code? Why?
+// // countModel.set = function() {
+// //   return "hello";
+// // };
+
+// console.log(countModel.set(5));
+
+// countModel.set = function() {
+//   return "hello";
+// };
+
+// console.log(countModel.set());
+
+// console.log("count is", countModel.count());
+
+// // // Question 5
+// function CountModel() {
+//   this._count = 0;
+// };
+
+// CountModel.prototype.count = function() {
+//   return this._count;
+// };
+
+// CountModel.prototype.set = function(count) {
+//   this._count = count;
+// };
+
+// var countModel = new CountModel();
+// countModel.set(5);
+// console.log("count is", countModel.count());
+
+// // Bonus research project. Can you find the property name below that
+// // makes the statement print out `true`?
+// //console.log(countModel["REPLACE_ME"] === CountModel.prototype);
+// console.log(CountModel.prototype)
+// console.log(countModel["__proto__"] === CountModel.prototype);
+
+
+//  //User stories shepherd
+
+function Pen(){
+  this.sheepies = [];
+
+  this.herd = function(sheep) {
+    this.sheepies.push(sheep)
+    return 'herded'
+  }
+}
+
+// Pen.prototype.herd = function(sheep) {
+//   this.sheepies.push(sheep)
+//   return 'herded'
+// }
+
+
+Pen. prototype.list = function() {
+  this.sheepies.forEach(function(sheep){
+    console.log(sheep.name)
+  })
+}
+
+
+function Sheep(name) {
+  this.name = name
+}
+
+pen = new Pen();
+sheep1 = new Sheep('one');
+sheep2 = new Sheep('two');
+
+pen.herd(sheep1)
+pen.herd(sheep2)
+
+pen.list()
